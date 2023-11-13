ac8fd13a8258358021350ee8152e37b5677028f9
yangharry <myid7771@naver.com>
2023-11-13 15:27:22 +0900

    git arrow files

---
+++ my-app/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { RootState } from './reducers';
+import { useSelector, useDispatch } from 'react-redux';
+
+type Props = {
+  value: any;
+  onIncrement: () => void;
+  onDecrement: () => void;
+};
+
+function App({ value, onIncrement, onDecrement }: Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+  const [todoValue, setTodoValue] = useState('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_TODO', text: todoValue });
+    setTodoValue('');
+  };
+  return (
+    <div className="App">
+      Clinked: {counter} times
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
